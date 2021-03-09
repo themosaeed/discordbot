6f4a950afc04e81bda084629ae5332d42deaa14a
codingontheway <mohammed.aly@vodafone.com>
2021-03-09 16:40:06 +0200

    added afifi

--- commands/birthday.js
+++ commands/birthday.js
@@ -1,6 +1,6 @@
-function birthday(arg, msg, zezoEmoji) {
-  if (arg === 'all') {
-    msg.reply(`1/2 :flag_tr: 
+function birthday(arg, msg, zezoEmoji, AfifiEmoji) {
+    if (arg === 'all') {
+        msg.reply(`1/2 :flag_tr: 
       10/2 :ninja: 
       9/3 :printer:
       10/5 ${zezoEmoji} 
@@ -12,76 +12,82 @@ function birthday(arg, msg, zezoEmoji) {
       1/9 :rabbit:
       7/11 :computer:
       20/11 :mag:
+      01/12 : ${AfifiEmoji}
       21/12 :rolling_eyes:
-      25/12 :soccer:`)
-  }
+      25/12 :soccer:`);
+    }
 
-  if (arg == 'zezo') {
-    console.log('wasalt')
-    msg.reply(
-      `zezo is a special person who came to life on the 10th of may giving life a new life`
-    )
-  }
-  if (arg === 'gebna') {
-    msg.reply(
-      `gebna is a special person who came to life on the 10th of july giving life a new type of gebna`
-    )
-  }
-  if (arg === 'sherlock') {
-    msg.reply(
-      `sherlock is a special person who came to life on the 20th of November giving life a new detective korombo`
-    )
-  }
-  if (arg === 'salma') {
-    msg.reply(
-      `salma is a special person who came to life on the 9th of March giving life a new rocket league professional `
-    )
-  }
-  if (arg === 'roma') {
-    msg.reply(
-      `roma is a special creature who came to life on the 1st of September giving life a new cute bunny`
-    )
-  }
-  if (arg === 'sheda') {
-    msg.reply(
-      `sheda is a special person who came to life on the 25th of December giving life a new fifa legend`
-    )
-  }
-  if (arg === 'taha') {
-    msg.reply(
-      `taha is a special person who came to life on the 21st of December giving life a new engineering legend`
-    )
-  }
-  if (arg === 'alaa') {
-    msg.reply(
-      `alaa is a special person who came to life on the 25th of july giving life a new super engineer and sambosa cooker`
-    )
-  }
-  if (arg === 'kero') {
-    msg.reply(
-      `Kero is a special person who came to life on the 26th of june giving life a new keroptonite`
-    )
-  }
-  if (arg === 'shakboni') {
-    msg.reply(
-      `shakboni is a special person who came to life on the 10th of Feb giving life a new super Ninja`
-    )
-  }
-  if (arg === 'romy') {
-    msg.reply(
-      `Romy is a special person who came to life on the 1st of Feb ( maybe 10th ) giving life a new way of saying oskot`
-    )
-  }
-  if (arg === 'jane') {
-    msg.reply(
-      `salma is a special person who came to life on the 12th of August giving life a new kofta lover`
-    )
-  }
-  if (arg === 'juba') {
-    msg.reply(
-      `Juba is a special person who came to life on the 12th of August giving life a new hamada helal`
-    )
-  }
+    if (arg == 'zezo') {
+        console.log('wasalt');
+        msg.reply(
+            `zezo is a special person who came to life on the 10th of may giving life a new life`
+        );
+    }
+    if (arg === 'gebna') {
+        msg.reply(
+            `gebna is a special person who came to life on the 10th of july giving life a new type of gebna`
+        );
+    }
+    if (arg === 'sherlock') {
+        msg.reply(
+            `sherlock is a special person who came to life on the 20th of November giving life a new detective korombo`
+        );
+    }
+    if (arg === 'salma') {
+        msg.reply(
+            `salma is a special person who came to life on the 9th of March giving life a new rocket league professional `
+        );
+    }
+    if (arg === 'roma') {
+        msg.reply(
+            `roma is a special creature who came to life on the 1st of September giving life a new cute bunny`
+        );
+    }
+    if (arg === 'sheda') {
+        msg.reply(
+            `sheda is a special person who came to life on the 25th of December giving life a new fifa legend`
+        );
+    }
+    if (arg === 'taha') {
+        msg.reply(
+            `taha is a special person who came to life on the 21st of December giving life a new engineering legend`
+        );
+    }
+    if (arg === 'alaa') {
+        msg.reply(
+            `alaa is a special person who came to life on the 25th of july giving life a new super engineer and sambosa cooker`
+        );
+    }
+    if (arg === 'kero') {
+        msg.reply(
+            `Kero is a special person who came to life on the 26th of june giving life a new keroptonite`
+        );
+    }
+    if (arg === 'shakboni') {
+        msg.reply(
+            `shakboni is a special person who came to life on the 10th of Feb giving life a new super Ninja`
+        );
+    }
+    if (arg === 'romy') {
+        msg.reply(
+            `Romy is a special person who came to life on the 1st of Feb ( maybe 10th ) giving life a new way of saying oskot`
+        );
+    }
+    if (arg === 'jane') {
+        msg.reply(
+            `salma is a special person who came to life on the 12th of August giving life a new kofta lover`
+        );
+    }
+    if (arg === 'juba') {
+        msg.reply(
+            `Juba is a special person who came to life on the 7th of november giving life a new hamada helal`
+        );
+    }
+    if (arg === 'afifi') {
+        msg.reply(
+            `afifi is a special person who came to life on the 1st of December giving life ${AfifiEmoji}`
+        );
+    }
 }
 
-module.exports = birthday
+module.exports = birthday;
--- utils/birthdayReminder.js
+++ utils/birthdayReminder.js
@@ -1,38 +1,40 @@
 function bdReminder(client, channel) {
-  const chn = client.channels.cache.get(channel)
+    const chn = client.channels.cache.get(channel);
 
-  // happyBirthday(chn, 'Romy', 2021, 1, 10, 14, 33)
-  // happyBirthday(chn, 'Samurai Shak', 2021, 1, 10, 14, 30)
-  happyBirthday(chn, '<@739455286825320468>', 2021, 2, 9, 12, 1) //salma
-  happyBirthday(chn, '<@316208244521697301>', 2021, 4, 10, 12, 1) //zezo
-  happyBirthday(chn, '<@403824108682346497>', 2021, 4, 16, 12, 1) //MO
-  happyBirthday(chn, '<@203590534126764033>', 2021, 5, 26, 12, 1) //kero
-  happyBirthday(chn, '<@327801712167354389>', 2021, 6, 10, 12, 1) //gobna
-  happyBirthday(chn, '<@616359831376363530>', 2021, 6, 25, 12, 1) //Alaa
-  happyBirthday(chn, '<@141298305626800128>', 2021, 7, 12, 12, 1) //Jane
-  happyBirthday(chn, '<@661348681517105152>', 2021, 8, 1, 12, 1) //Roma
-  happyBirthday(chn, '<@278273060811964416>', 2021, 10, 7, 12, 1) //juba
-  happyBirthday(chn, '<@694062315888181309>', 2021, 10, 20, 12, 1) //sherlock
-  happyBirthday(chn, '<@334184503578984448>', 2021, 11, 21, 12, 1) //Taha
-  happyBirthday(chn, '<@696775564748390491>', 2021, 11, 25, 12, 1) //Sheda
+    // happyBirthday(chn, 'Romy', 2021, 1, 10, 14, 33)
+    // happyBirthday(chn, 'Samurai Shak', 2021, 1, 10, 14, 30)
+    //happyBirthday(chn, '<@739455286825320468>', 2021, 2, 9, 12, 1) //salma
+    happyBirthday(chn, '<@316208244521697301>', 2021, 4, 9, 22, 1); //zezo
+    happyBirthday(chn, '<@403824108682346497>', 2021, 4, 15, 22, 1); //MO
+    happyBirthday(chn, '<@203590534126764033>', 2021, 5, 25, 22, 1); //kero
+    happyBirthday(chn, '<@327801712167354389>', 2021, 6, 9, 22, 1); //gobna
+    happyBirthday(chn, '<@616359831376363530>', 2021, 6, 24, 22, 1); //Alaa
+    happyBirthday(chn, '<@141298305626800128>', 2021, 7, 11, 22, 1); //Jane
+    happyBirthday(chn, '<@661348681517105152>', 2021, 8, 1, 1, 1); //Roma
+    happyBirthday(chn, '<@278273060811964416>', 2021, 10, 6, 22, 1); //juba
+    happyBirthday(chn, '<@694062315888181309>', 2021, 10, 19, 22, 1); //sherlock
+    happyBirthday(chn, '<@145210621787504640>', 2021, 11, 1, 1, 1); //afifi
+    happyBirthday(chn, '<@334184503578984448>', 2021, 11, 20, 22, 1); //Taha
+    happyBirthday(chn, '<@696775564748390491>', 2021, 11, 24, 22, 1); //Sheda
 }
 
 function happyBirthday(chn, person, year, month, day, hour, minute) {
-  const date = new Date(year, month, day, hour, minute).getTime() - Date.now()
-  const diff = Math.max(date, 0)
-  if (diff > 0x7fffffff)
-    setTimeout(
-      () => HappyBirthday(chn, person, year, month, day, hour, minute),
-      0x7fffffff
-    )
-  else
-    setTimeout(
-      () =>
-        chn.send(
-          `:partying_face: :partying_face: :partying_face:  @everyone Today is the day and it's a birthday.  Happy Birthday ${person}   :partying_face: :partying_face: :partying_face: `
-        ),
-      diff
-    )
+    const date =
+        new Date(year, month, day, hour, minute).getTime() - Date.now();
+    const diff = Math.max(date, 0);
+    if (diff > 0x7fffffff)
+        setTimeout(
+            () => HappyBirthday(chn, person, year, month, day, hour, minute),
+            0x7fffffff
+        );
+    else
+        setTimeout(
+            () =>
+                chn.send(
+                    `:partying_face: :partying_face: :partying_face:  @everyone Today is the day and it's a birthday.  Happy Birthday ${person}   :partying_face: :partying_face: :partying_face: `
+                ),
+            diff
+        );
 }
 
-module.exports = bdReminder
+module.exports = bdReminder;
